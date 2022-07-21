6e6b6415f34ad2583a85b848e05d324205e65965
Vladislav529 <iemm@rambler.ru>
2022-07-22 00:16:48 +0300

    get data from backend

--- src/pages/reviews/Reviews.js
+++ src/pages/reviews/Reviews.js
@@ -2,44 +2,68 @@ import React from "react";
 import axios from 'axios';
 import { Row, Col, Button } from "reactstrap";
 import CircleProgress from '../../components/CircleProgress/'
-import SquareProgress from '../../components/SquareProgress/'
 import Widget from "../../components/Widget";
 import s from './Reviews.module.scss';
 
-function setInfo() {
-	let standartText = document.getElementsByClassName(`${s.percentage}`);
-	let classPercentage = s.percentage;
-	CircleProgress(`.${s.counter}`, standartText, classPercentage);
-	console.log('onload');
-};
-
-let state;
 class Reviews extends React.Component {
-	showState() {
-		console.log(state);
+	constructor(props) {
+		super(props);
+		this.state = {response: []};
 	}
+
 	componentDidMount() {
 		let headers = {
 			'Access-Control-Allow-Origin' : '*'
 		}
-		axios.get('https://reviews-ai.ru/api/v1/getreviews/?offset=0&limit=50', headers).then( res => {
-      		state = ({ response: res.data.response });
+		axios.get('https://reviews-ai.ru/api/v1/getreviews/?offset=0&limit=20', headers).then( res => {
+      		this.setState({ response: res.data.response });
     	})
-    	setTimeout(() => {
-    		
-    	}, 500);
 	}
-	render() {
 
-		this.showState()
+	componentDidUpdate() {
+		let standartText, classPercentage;
+		try {
+			standartText = document.getElementsByClassName(`${s.percentage}`);
+			classPercentage = s.percentage;
+		} catch {
+			return;
+		}
+		CircleProgress(`.${s.counter}`, standartText, classPercentage);
+	}
+
+	render() {
+		if (Object.keys(this.state.response).length == 0) {
+			return (<div>
+				<h1 className="page-title">
+					Отзывы
+				</h1>
+			</div>);
+		}
+		console.log(this.state.response.map((body, item) => body))
 		return (
 			<div>
 				<h1 className="page-title">
 					Отзывы
 				</h1>
-				{state.map((body, index) => {
-					body.rating
-				})}
+				{this.state.response.map((body, item) =>
+				<Widget>
+					<div className={s.review}>
+						<div className={s.text}>
+							<h4>{body.username} <span className={s.date}>{body.publication_date.split('T')[0].split('-').reverse().join('.')}</span></h4>
+							<p>{body.review}</p>
+						</div>
+						<div className={s.data}>
+							<div className={s.rating}>
+								<p className={s.descriptionAnalysis}>Общая оценка</p>
+								<div className={s.counter} data-cp-percentage={body.rating * 5}></div>
+							</div>
+							<div className={s.categories}>
+								<div className={s.bank}>Bank</div>
+							</div>
+						</div>
+					</div>
+				</Widget>
+				)}
 				<Widget>
 					<div className={s.review}>
 						<div className={s.text}>
